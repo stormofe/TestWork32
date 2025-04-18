a1e3811fe7cd43c66a1be1f4fa90eb8df505c019
Alexandra <stormofe@gmail.com>
2025-04-18 13:03:00 +0400

    configurate alias

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+      '@ui': path.resolve(__dirname, 'src/components/ui'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
